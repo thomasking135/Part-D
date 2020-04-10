5303b6401b517836fb96a55386f29280a732886d
Tom E King <62085061+thomasking135@users.noreply.github.com>
2020-04-11 07:17:11 +1200

    This is my 4th commit
    Yes, I know Monday is the least favourite day for most people. But I'm philosophical in believing your alive and ready to face another week of existence :)

--- BIT504Assignment2GITHUBCommit/src/PartD.java
+++ BIT504Assignment2GITHUBCommit/src/PartD.java
@@ -6,6 +6,7 @@ public class PartD {
 System.out.println("Hi, my name is Thomas");
 System.out.println("MY favourite colour is Azzurri blue");
 System.out.println("My favourite mode of transport is rollerblading");
+System.out.println("The best day of the week is Monday");
 	}
 
 }
