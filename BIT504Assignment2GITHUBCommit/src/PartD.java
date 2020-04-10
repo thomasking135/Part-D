bd8cece8c26844791a14538d9826a49edba9973d
Tom E King <62085061+thomasking135@users.noreply.github.com>
2020-04-11 07:09:30 +1200

    This is the updated file now showing my favourite colour!
    I love Azzurri blue!

--- BIT504Assignment2GITHUBCommit/src/PartD.java
+++ BIT504Assignment2GITHUBCommit/src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hi, my name is Thomas");
+System.out.println("MY favourite colour is Azzurri blue");
 	}
 
 }
