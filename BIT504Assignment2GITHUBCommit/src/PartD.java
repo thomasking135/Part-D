24870b85fbc827331a0b11f32e335073f91110ff
Tom E King <62085061+thomasking135@users.noreply.github.com>
2020-04-11 07:14:31 +1200

    This is my new amended file showing my favourite mode of transport.
    Rollerblading is a favourite pastime of mine. I was introduced to it by an American friend staying in our house. He was crazy about roller derbies and roller hockey!

--- BIT504Assignment2GITHUBCommit/src/PartD.java
+++ BIT504Assignment2GITHUBCommit/src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		// TODO Auto-generated method stub
 System.out.println("Hi, my name is Thomas");
 System.out.println("MY favourite colour is Azzurri blue");
+System.out.println("My favourite mode of transport is rollerblading");
 	}
 
 }
